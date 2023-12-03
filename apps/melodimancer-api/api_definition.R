210d5a7a07d8bf1e37a2d93589a35bb1393fd7f3
SirKiels <samuelvankiel@gmail.com>
2023-12-03 16:58:27 +0100

    Update api_definition.R

--- apps/melodimancer-api/api_definition.R
+++ apps/melodimancer-api/api_definition.R
@@ -1,21 +1,117 @@
-#plumber.R
-library(plumber)
-
-generate_random_text <- function(length) {
-  # Define the character set
-  characters <- c(letters, LETTERS, 0:9, "!@#$%^&*()_-+=<>?")
-  
-  # Generate random text of the specified length
-  random_text <- paste(sample(characters, length, replace = TRUE), collapse = "")
-  
-  return(random_text)
+# Install and load the plumber library
+# install.packages("plumber")
+{library(plumber)
+  library(jsonlite)
+  library(lavaan)
+  library(corrplot)
+  library(ggplot2)
+  library(psych)
+  library(tidyr)
+  library(gridExtra)
+  library(spotifyr)
 }
 
-# Generate and print random text of length 10
-random_text <- generate_random_text(10)
+# Custom functions
+{
+  # F1 turn json into R DF
+  F1_json_df <- function(input_data) {
+    df1 <- fromJSON(input_data)
+    return(df1)
+  }
+  
+  # F2 turns F1 output into summary statistics as R table and JSON
+  F2_summary_table <- function(df1) {
+    component_names <- c(
+      "Acousticness",
+      "Danceability",
+      "Liveness",
+      "Instrumentalness",
+      "Energy",
+      "Loudness",
+      "Speechiness",
+      "Tempo",
+      "Valence"
+    )
+    
+    summary_table <- data.frame(
+      Components = character(),
+      Median = numeric(),
+      Mean = numeric(),
+      Q1 = numeric(),
+      Q1low30pc = numeric(),
+      Q1low20pc = numeric(),
+      Q1low10pc = numeric(),
+      Q1high30pc = numeric(),
+      Q1high20pc = numeric(),
+      Q1high10pc = numeric(),
+      Q3 = numeric(),
+      Q3low30pc = numeric(),
+      Q3low20pc = numeric(),
+      Q3low10pc = numeric(),
+      Q3high30pc = numeric(),
+      Q3high20pc = numeric(),
+      Q3high10pc = numeric(),
+      stringsAsFactors = FALSE
+    )
+    
+    for (i in 1:length(component_names)) {
+      column <- df1[[component_names[i]]]
+      column_median <- median(column, na.rm = TRUE)
+      column_mean <- mean(column, na.rm = TRUE)
+      column_q1 <- quantile(column, 0.25, na.rm = TRUE)
+      column_q1low30pc <- column_q1 * 0.7
+      column_q1low20pc <- column_q1 * 0.8
+      column_q1low10pc <- column_q1 * 0.9
+      column_q1high30pc <- column_q1 * 1.3
+      column_q1high20pc <- column_q1 * 1.2
+      column_q1high10pc <- column_q1 * 1.1
+      column_q3 <- quantile(column, 0.75, na.rm = TRUE)
+      column_q3low30pc <- column_q3 * 0.7
+      column_q3low20pc <- column_q3 * 0.8
+      column_q3low10pc <- column_q3 * 0.9
+      column_q3high30pc <- column_q3 * 1.3
+      column_q3high20pc <- column_q3 * 1.2
+      column_q3high10pc <- column_q3 * 1.1
+      
+      new_row <- list(
+        Components = component_names[i],
+        Median = column_median,
+        Mean = column_mean,
+        Q1 = column_q1,
+        Q1low30pc = column_q1low30pc,
+        Q1low20pc = column_q1low20pc,
+        Q1low10pc = column_q1low10pc,
+        Q1high30pc = column_q1high30pc,
+        Q1high20pc = column_q1high20pc,
+        Q1high10pc = column_q1high10pc,
+        Q3 = column_q3,
+        Q3low30pc = column_q3low30pc,
+        Q3low20pc = column_q3low20pc,
+        Q3low10pc = column_q3low10pc,
+        Q3high30pc = column_q3high30pc,
+        Q3high20pc = column_q3high20pc,
+        Q3high10pc = column_q3high10pc
+      )
+      summary_table <- rbind(summary_table, new_row)
+    }
+    
+    # Convert data frame to JSON
+    summary_json <- toJSON(summary_table, pretty = TRUE)
+    
+    return(list(summary_table = summary_table, summary_json = summary_json))
+  }
+  
+  # F3 turns 
+  
+}
 
-#* @get /random_text
-#* @param l
-function(l) {
-  generate_random_text(l)
+#* @post /SummaryTable
+#* Spotify top 100 as JSON
+#* @param input_data Input top 100 as JSON
+#* @serializer json
+function(input_data) {
+  result <- F1_json_df(input_data)
+  SumTableAndJSON <- F2_summary_table(result)
+  SumTableAndJSON$summary_table
+  #SumTableAndJSON$summary_json
 }
