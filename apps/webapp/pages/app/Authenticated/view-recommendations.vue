00cac0ec98f404672466a2c584c58266f7c00ed0
MartipeCode <130598382+MartipeCode@users.noreply.github.com>
2024-10-23 13:51:43 +0200

    Update authenticated page (#2)
    * Update-Authenticated-Page
    
    Updates to the authenticated page to be a simple on button functionality
    
    * More stuff for authenticated flow
    
    ---------
    
    Co-authored-by: valtechmartin <martin.pedersen@valtech.com>

---
+++ apps/webapp/pages/app/Authenticated/view-recommendations.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: 'admin',
+  middleware: ['user-data-populated'],
+})
+
+const recommendationsStore = useRecommendationsStore()
+</script>
+
+<template>
+  <div>
+    <NuxtLink to="/app/">Back to app</NuxtLink>
+    <!--<div>
+      <h2>Payload:</h2>
+      <VueJsonPretty
+        v-if="recommendationsStore.payload"
+        :data="recommendationsStore.payload"
+      />
+    </div>-->
+    <div v-if="recommendationsStore.recommendations.length">
+      <FeatureCreatePlaylist />
+    </div>
+    <div>
+      <h2>
+        Recommended Tracks: {{ recommendationsStore.recommendations.length }}
+      </h2>
+      <ul style="max-width: 600px">
+        <li
+          v-for="track in recommendationsStore.recommendations"
+          :key="track.id"
+          style="margin-bottom: 20px; list-style: none"
+        >
+          <ModuleTrackTile :track="track" />
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
--- apps/webapp/pages/app/index.vue
+++ apps/webapp/pages/app/index.vue
@@ -1,9 +1,62 @@
+<script setup lang="ts">
+import Admin from '~/layouts/admin.vue';
+
+definePageMeta({
+  layout: 'authenticated',
+})
+const props = defineProps<{
+  title: string
+  seedType: SeedTypes
+}>()
+
+const userDataStore = useUserDataStore()
+const recommendationsStore = useRecommendationsStore()
+
+
+const layout = inject('layout') as AvailableLayouts | undefined
+
+async function handleRecommendationClick() {
+  await recommendationsStore.getRecommendations({ seedType: 'track' })
+  navigateTo(buildAppUrl('/app/view-recommendations', layout))
+}
+
+const timeRange = 'short_term'
+async function test() {
+  try {
+    await userDataStore.getUserData(timeRange)
+  } catch (err) {
+    console.log(err)
+  }
+
+  let test2 = userDataStore.topTracks[0].id
+  console.log(test2)
+
+  recommendationsStore.tracksIds = []
+  recommendationsStore.updateSeed('track', test2)
+  recommendationsStore.updateFactors({
+    useFactor: 'True',
+    factor: 'new_RC1',
+    threshold:"",
+  })
+
+  
+
+  handleRecommendationClick()
+}
+</script>
 <template>
-  <div>
-    <h1>Jake needs to create this page</h1>
+
+<div class="main">
+  <div class="authentic">
+    <h1>Click here to instantly get recommendations</h1>
     <p>
-      When creating pages outside the admin domain we need to use the
-      <code>authenticated</code> layout.
+      Recommendations will be presented as a playlist that can then be tailored.
     </p>
+    <ElButton
+      :title="`Get recommendations`"
+      @click="test"
+    />
   </div>
+</div>
+
 </template>
