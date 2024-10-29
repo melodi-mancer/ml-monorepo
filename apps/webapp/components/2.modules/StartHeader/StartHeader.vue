a2313f4a2cbeca26d31864ca395bb738d9ff2362
valtechmartin <martin.pedersen@valtech.com>
2024-10-29 16:18:18 +0100

    Update with a pre-login header

---
+++ apps/webapp/components/2.modules/StartHeader/StartHeader.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import './StartHeader.scss'
+
+const config = useRuntimeConfig()
+const authStore = useAuthStore()
+
+const version = config.public.appVersion
+</script>
+
+<template>
+  <div class="header">
+    <div class="title">
+      <div class="logo"></div>
+      <h1>Melodimancer</h1>
+    </div>
+    <div class="secondary">
+      <small>v{{ version }}</small>
+    </div>
+  </div>
+</template>
---
+++ apps/webapp/layouts/start.vue
@@ -0,0 +1,17 @@
+<script setup>
+provide('layout', 'start')
+</script>
+
+<template>
+  <div class="page-container">
+    <div class="top">
+      <ModuleStartHeader />
+    </div>
+    <div class="main">
+      <slot />
+    </div>
+    <div class="bottom">
+      <ModuleFooter />
+    </div>
+  </div>
+</template>
